d14b60e30756d78661af3ad6a2f7a0a64304b487
vr009 <slavarianov@yandex.ru>
2021-10-03 21:41:35 +0300

    preparation for deploy

--- postgres/10-create-user-and-db.sql
+++ postgres/10-create-user-and-db.sql
@@ -3,3 +3,11 @@ CREATE DATABASE persons;
 CREATE ROLE program WITH PASSWORD 'test';
 GRANT ALL PRIVILEGES ON DATABASE persons TO program;
 ALTER ROLE program WITH LOGIN;
+
+CREATE TABLE persons (
+    person_id bigserial not null primary key,
+    name text not null,
+    age integer not null,
+    work text,
+    address text not null
+);
--- postgres/init_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE persons (
-    person_id bigserial not null primary key,
-    name text not null,
-    age integer not null,
-    work text,
-    address text not null
-);
