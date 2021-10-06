dca2be284d7ffff245cce4a13983a8a0d27a0f4e
vr009 <slavarianov@yandex.ru>
2021-10-06 20:24:06 +0300

    temporary repaired

--- service/middleware/cors.go
+++ service/middleware/cors.go
@@ -4,13 +4,13 @@ import "net/http"
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Methods", "POST,PATCH,DELETE,GET")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-CSRF-Token")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("*"))
-		if r.Method == http.MethodOptions {
-			return
-		}
+		//w.Header().Set("Access-Control-Allow-Methods", "POST,PATCH,DELETE,GET")
+		//w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-CSRF-Token")
+		//w.Header().Set("Access-Control-Allow-Credentials", "true")
+		//w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("*"))
+		//if r.Method == http.MethodOptions {
+		//	return
+		//}
 		next.ServeHTTP(w, r)
 	})
 }
